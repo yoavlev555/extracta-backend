06e0aa963a72d2847ad8605e42a5072600556d11
Yoav Levin <y_levin@apple.com>
2026-01-07 20:53:04 +0200

    define documents routes in api

--- app/main_api.py
+++ app/main_api.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 
 from fastapi import FastAPI
+from fastapi_pagination import add_pagination
 
 from app.api.v1 import documents, users
 from app.core.config import config
@@ -13,8 +14,12 @@ app = FastAPI(title="Extracta backend API", version=config.version)
 _LOGGER = logging.getLogger()
 
 # Register routes
-app.include_router(documents.router)
-app.include_router(users.router)
+app.include_router(documents.router, prefix="/api/v1")
+app.include_router(users.router, prefix="/api/v1")
+
+# Add pagination support to the app
+# This must be done AFTER all routes are added if using routers
+add_pagination(app)
 
 
 @app.get("/health", tags=["Health"])
