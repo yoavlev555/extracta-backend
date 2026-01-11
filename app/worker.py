e8adcdbe1bee99b0ed3dd5673bad540e55aa157f
Liorbau <liorbau@gmail.com>
2026-01-11 16:49:48 +0200

    edited logger structure

--- app/worker.py
+++ app/worker.py
@@ -6,6 +6,7 @@ from models import Document
 
 from app.core.logger import logger
 
+logger = logger.init_logger(__name__)
 
 def start_processing_simulation(doc_id: str, db: list[Document]) -> None:
     thread = threading.Thread(target=_run_simulation, args=(doc_id, db))
@@ -18,19 +19,19 @@ def _run_simulation(doc_id: str, db: list[Document]) -> None:
     Worker logic that strictly follows: FETCH -> PROCESS -> UPDATE
     """
 
-    logger.info(f"[Worker] Job received for ID: {doc_id}")
+    logger.info(f"Job received for ID: {doc_id}")
     time.sleep(5)
 
     doc = next((document for document in db if document.id == doc_id), None)
 
     if not doc:
-        logger.error(f"[Worker] Error: Document {doc_id} doesn't exist")
+        logger.error(f"Error: Document {doc_id} doesn't exist")
         return
 
     doc.status = Status.PROCESSING
-    logger.info(f"[Worker] Processing {doc_id}...")
+    logger.info(f"Processing {doc_id}...")
 
     time.sleep(10)
 
     doc.status = Status.SUCCESS
-    logger.info(f"[Worker] {doc_id} is ready!")
+    logger.info(f"{doc_id} is ready!")
